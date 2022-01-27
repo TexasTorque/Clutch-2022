f4cca57bffd9108818999a6afcf500be71642614
realSaddy <jrp30074@gmail.com>
2022-01-27 12:21:20 -0600

    Rotate manager initial draft

--- src/main/java/org/texastorque/constants/Constants.java
+++ src/main/java/org/texastorque/constants/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
     public static final double DRIVE_WHEEL_RADIUS_METERS = -1;
     public static final double DRIVE_MAX_SPEED_METERS = -1;
     public static final double DRIVE_MAX_ANGUAR_SPEED_RADIANS = -1;
+
+    public static final double ROTATE_MANAGER_PID_P = 0;
+    public static final double ROTATE_MANAGER_PID_I = 0;
+    public static final double ROTATE_MANAGER_PID_D = 0;
 }

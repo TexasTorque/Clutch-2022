f0a57543ef2731e428980f1a8d7cfac6a11d1816
realSaddy <jrp30074@gmail.com>
2022-03-04 08:49:30 -0600

    Fix paths

--- src/main/java/org/texastorque/auto/sequences/mode2/Mode2Right.java
+++ src/main/java/org/texastorque/auto/sequences/mode2/Mode2Right.java
@@ -17,11 +17,11 @@ public class Mode2Right extends TorqueSequence {
     @Override
     protected void init() {
         // Start up mag
-        // addBlock(new TorqueBlock(
-        // new SetMagazine(BeltDirections.INTAKE, GateSpeeds.CLOSED)));
+        addBlock(new TorqueBlock(
+                new SetMagazine(BeltDirections.INTAKE, GateSpeeds.CLOSED)));
 
         // Shoot
-        // addBlock(new TorqueBlock(new ShootAtTarget()));
+        addBlock(new TorqueBlock(new ShootAtTarget()));
 
         // Taxi
         addBlock(new TorqueBlock(new Pathplanner("Mode2Right")));

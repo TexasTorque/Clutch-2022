c07abb76c5d8e96fb2eceeda6b75ce8b48a50c7c
realSaddy <jrp30074@gmail.com>
2022-03-17 10:23:57 -0500

    bump pathplanner

--- src/main/java/org/texastorque/auto/commands/Pathplanner.java
+++ src/main/java/org/texastorque/auto/commands/Pathplanner.java
@@ -22,29 +22,28 @@ public class Pathplanner extends TorqueCommand {
     private PathPlannerTrajectory trajectory;
 
     private final PIDController xController = new PIDController(
-        Constants.PATH_PLANNER_X_P, Constants.PATH_PLANNER_X_I,
-        Constants.PATH_PLANNER_X_D);
+            Constants.PATH_PLANNER_X_P, Constants.PATH_PLANNER_X_I,
+            Constants.PATH_PLANNER_X_D);
     private final PIDController yController = new PIDController(
-        Constants.PATH_PLANNER_Y_P, Constants.PATH_PLANNER_Y_I,
-        Constants.PATH_PLANNER_Y_D);
-    private final TrapezoidProfile.Constraints tConstraints =
-        new TrapezoidProfile.Constraints(Constants.MAX_ANGULAR_SPEED,
-                                         Constants.MAX_ANGULAR_ACCELERATION);
-    private final ProfiledPIDController thetaController =
-        new ProfiledPIDController(Constants.PATH_PLANNER_R_P,
-                                  Constants.PATH_PANNER_R_I,
-                                  Constants.PATH_PLANNER_R_D, tConstraints);
-    private final HolonomicDriveController hController =
-        new HolonomicDriveController(xController, yController, thetaController);
+            Constants.PATH_PLANNER_Y_P, Constants.PATH_PLANNER_Y_I,
+            Constants.PATH_PLANNER_Y_D);
+    private final TrapezoidProfile.Constraints tConstraints = new TrapezoidProfile.Constraints(
+            Constants.MAX_ANGULAR_SPEED,
+            Constants.MAX_ANGULAR_ACCELERATION);
+    private final ProfiledPIDController thetaController = new ProfiledPIDController(Constants.PATH_PLANNER_R_P,
+            Constants.PATH_PANNER_R_I,
+            Constants.PATH_PLANNER_R_D, tConstraints);
+    private final HolonomicDriveController hController = new HolonomicDriveController(xController, yController,
+            thetaController);
     private final Timer timer = new Timer();
 
     private boolean resetOdometry = true;
 
     public Pathplanner(String name) {
         thetaController.enableContinuousInput(Math.toRadians(-180),
-                                              Math.toRadians(180));
+                Math.toRadians(180));
         trajectory = PathPlanner.loadPath(name, Constants.TOP_SPEED_METERS,
-                                          Constants.TOP_ACCELERATION_METERS);
+                Constants.TOP_ACCELERATION_METERS);
     }
 
     public Pathplanner(String name, boolean resetOdometry) {
@@ -59,54 +58,50 @@ public class Pathplanner extends TorqueCommand {
         timer.start();
 
         if (resetOdometry) {
-            float offset =
-                360f -
-                (float)trajectory.getInitialPose().getRotation().getDegrees();
+            float offset = 360f -
+                    (float) trajectory.getInitialPose().getRotation().getDegrees();
             Feedback.getInstance().getGyroFeedback().setAngleOffset(offset);
             Drivebase.getInstance().odometry.resetPosition(
-                trajectory.getInitialPose(),
-                trajectory.getInitialPose().getRotation());
+                    trajectory.getInitialPose(),
+                    trajectory.getInitialPose().getRotation());
 
-            System.out.println("My initial: " + Drivebase.getInstance()
-                                                    .odometry.getPoseMeters()
-                                                    .getRotation()
-                                                    .getDegrees());
+            System.out.println("My initial: " + Drivebase.getInstance().odometry.getPoseMeters()
+                    .getRotation()
+                    .getDegrees());
             System.out.println("My initial real: " + Feedback.getInstance()
-                                                         .getGyroFeedback()
-                                                         .getRotation2d()
-                                                         .getDegrees());
+                    .getGyroFeedback()
+                    .getRotation2d()
+                    .getDegrees());
             System.out.println(
-                "Wanted initial:" +
-                trajectory.getInitialPose().getRotation().getDegrees());
+                    "Wanted initial:" +
+                            trajectory.getInitialPose().getRotation().getDegrees());
         }
     }
 
     @Override
     protected void continuous() {
         double time = timer.get();
-        PathPlannerState currentTrajectory =
-            (PathPlannerState)trajectory.sample(time);
+        PathPlannerState currentTrajectory = (PathPlannerState) trajectory.sample(time);
 
         SmartDashboard.putNumber("[Want]X",
-                                 currentTrajectory.poseMeters.getX());
+                currentTrajectory.poseMeters.getX());
         SmartDashboard.putNumber("[Want]Y",
-                                 currentTrajectory.poseMeters.getY());
+                currentTrajectory.poseMeters.getY());
         SmartDashboard.putNumber(
-            "[Want]Rot",
-            currentTrajectory.poseMeters.getRotation().getDegrees());
+                "[Want]Rot",
+                currentTrajectory.poseMeters.getRotation().getDegrees());
 
         ChassisSpeeds chassisSpeeds = hController.calculate(
-            Drivebase.getInstance().odometry.getPoseMeters(), currentTrajectory,
-            currentTrajectory.holonomicRotation);
+                Drivebase.getInstance().odometry.getPoseMeters(), currentTrajectory,
+                currentTrajectory.holonomicRotation);
         chassisSpeeds = new ChassisSpeeds(-chassisSpeeds.vxMetersPerSecond,
-                                          chassisSpeeds.vyMetersPerSecond,
-                                          chassisSpeeds.omegaRadiansPerSecond);
-        SwerveModuleState[] states =
-            Drivebase.getInstance().kinematics.toSwerveModuleStates(
+                chassisSpeeds.vyMetersPerSecond,
+                chassisSpeeds.omegaRadiansPerSecond);
+        SwerveModuleState[] states = Drivebase.getInstance().kinematics.toSwerveModuleStates(
                 chassisSpeeds);
 
         SwerveDriveKinematics.desaturateWheelSpeeds(states,
-                                                    Constants.TOP_SPEED_METERS);
+                Constants.TOP_SPEED_METERS);
 
         // Set the states
         AutoInput.getInstance().setDriveStates(states);
@@ -121,8 +116,8 @@ public class Pathplanner extends TorqueCommand {
     protected void end() {
         timer.stop();
         AutoInput.getInstance().setDriveStates(
-            Drivebase.getInstance().kinematics.toSwerveModuleStates(
-                new ChassisSpeeds(0, 0, 0)));
+                Drivebase.getInstance().kinematics.toSwerveModuleStates(
+                        new ChassisSpeeds(0, 0, 0)));
         System.out.println("Pathplanner done: Have a great day!");
     }
 }

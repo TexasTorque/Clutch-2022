df06706d0e78e4ad50ee8200748fc38e7e5feec5
realSaddy <jrp30074@gmail.com>
2022-03-29 18:29:42 -0500

    Fix merge conflicts

--- src/main/java/org/texastorque/auto/commands/ShootConst.java
+++ src/main/java/org/texastorque/auto/commands/ShootConst.java
@@ -18,65 +18,32 @@ import org.texastorque.torquelib.auto.TorqueCommand;
 public class ShootConst extends TorqueCommand {
     private final double magOutputTime;
 
-<<<<<<< HEAD
-    private boolean stop = false, done = false, runMag = false;
-    private double rpm, hood, turret, startMagTime = 0;
-=======
     private boolean stop = false;
     private boolean done = false;
     private boolean runMag = false;
     private double startMagTime = 0;
     private double rpm, hood, turret;
->>>>>>> 8f52bb6445a34761be5bb2e59df29c37385793e3
 
     private int readyIterations = 0;
     private final int neededReadyIterations = 4;
 
     public ShootConst(double rpm, double hood, double turret) {
-<<<<<<< HEAD
-        this.rpm = rpm;
-        this.hood = hood;
-        this.turret = turret;
-        this.stop = false;
-        this.magOutputTime = 1;
-    }
-
-    public ShootConst(double rpm, double hood, double turret, boolean stop) {
-        this.rpm = rpm;
-        this.hood = hood;
-        this.turret = turret;
-        this.stop = stop;
-        this.magOutputTime = 1;
-    }
-
-    public ShootConst(double rpm, double hood, double turret, boolean stop, double magOutputTime) {
-        this.rpm = rpm;
-        this.hood = hood;
-        this.turret = turret;
-        this.stop = stop;
-=======
-        ShootConst(rpm, hood, turret, 2.);
+        this(rpm, hood, turret, 2.);
     }
 
     public ShootConst(double rpm, double hood, double turret, double magOutputTime) {
         this.rpm = rpm;
         this.hood = hood;
         this.turret = turret;
->>>>>>> 8f52bb6445a34761be5bb2e59df29c37385793e3
         this.magOutputTime = magOutputTime;
     }
 
     @Override
     protected void init() {
-<<<<<<< HEAD
         AutoInput.getInstance().setSetTurretPosition(true);
         AutoInput.getInstance().setTurretPosition(turret);
         AutoInput.getInstance().setHoodPosition(hood);
         AutoInput.getInstance().setFlywheelSpeed(rpm);
-=======
-        AutoInput.getInstance().setSetTurretPosition(false);
-
->>>>>>> 8f52bb6445a34761be5bb2e59df29c37385793e3
     }
 
     @Override
@@ -88,10 +55,6 @@ public class ShootConst extends TorqueCommand {
                     // if so, launch magazine for x seconds
                     runMag = true;
                     startMagTime = Timer.getFPGATimestamp();
-<<<<<<< HEAD
-=======
-
->>>>>>> 8f52bb6445a34761be5bb2e59df29c37385793e3
                 } else {
                     readyIterations++;
                 }
@@ -114,10 +77,6 @@ public class ShootConst extends TorqueCommand {
 
     @Override
     protected void end() {
-<<<<<<< HEAD
-=======
-        System.out.println("ShootAtTarget done, have a good day!");
->>>>>>> 8f52bb6445a34761be5bb2e59df29c37385793e3
         done = false;
         runMag = false;
         if (stop)

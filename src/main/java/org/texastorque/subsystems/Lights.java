ad1ebc9af1278cac458c0ec3991013dc138ac635
Juicestus <jus@gtsbr.org>
2022-04-01 22:48:43 -0500

    removed green during shooting

--- src/main/java/org/texastorque/subsystems/Lights.java
+++ src/main/java/org/texastorque/subsystems/Lights.java
@@ -87,13 +87,14 @@ public class Lights extends TorqueSubsystem {
 
     @Override
     public void updateTeleop() {
-        if (Feedback.getInstance().isTurretAlligned() && Feedback.getInstance().getShooterFeedback().getRPM() != 0)
-            // TARGET LOCK
-            setLights(0, 255, 0);
-        else if (Input.getInstance().getShooterInput().getFlywheel() != 0)
-            // SHOOTING
-            setLights(0, 255, 0, .25);
-        else if (Input.getInstance().getClimberInput().hasClimbStarted())
+        // if (Feedback.getInstance().isTurretAlligned() && Feedback.getInstance().getShooterFeedback().getRPM() != 0)
+        //     // TARGET LOCK
+        //     setLights(0, 255, 0);
+        // else if (Input.getInstance().getShooterInput().getFlywheel() != 0)
+        //     // SHOOTING
+        //     setLights(0, 255, 0, .25);
+        // else 
+        if (Input.getInstance().getClimberInput().hasClimbStarted())
             // ENDGAME RAINBOW
             setRainbow();
         else

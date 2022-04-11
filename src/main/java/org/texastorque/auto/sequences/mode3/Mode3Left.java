f36ae7659c100b3fef4e614ca404bcb52034d95c
realSaddy <jrp30074@gmail.com>
2022-04-11 07:37:41 -0500

    FIT last day commits

--- src/main/java/org/texastorque/auto/sequences/mode3/Mode3Left.java
+++ src/main/java/org/texastorque/auto/sequences/mode3/Mode3Left.java
@@ -19,8 +19,8 @@ public class Mode3Left extends TorqueSequence {
     protected void init() {
         // Start Intake, Automag
         addBlock(new TorqueBlock(
-                new PrepareTurret(-168.69),
-                new PrepareShooter(26, 1830),
+                new PrepareTurret(-165.69),
+                new PrepareShooter(30, 1900),
                 new SetIntake(IntakePosition.DOWN, IntakeDirection.INTAKE),
                 new SetMagazine(BeltDirections.INTAKE, GateSpeeds.CLOSED)));
 
@@ -29,7 +29,7 @@ public class Mode3Left extends TorqueSequence {
 
         // Shoot!
         addBlock(new TorqueBlock(new SetIntake(IntakePosition.PRIME, IntakeDirection.STOPPED),
-                new ShootConst(1830, 26, -168.69, true, 1.6)));
+                new ShootConst(1900, 30, -165.69, true, 1.6)));
 
         // Shut off
         addBlock(new TorqueBlock(new SetMagazine(BeltDirections.OFF, GateSpeeds.CLOSED)));

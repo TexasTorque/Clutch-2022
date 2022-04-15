249ed3f08b26183c702cc8877a826db423927242
Juicestus <jus@gtsbr.org>
2022-04-15 12:55:37 -0500

    .1 -> .3 (:

--- src/main/java/org/texastorque/constants/Constants.java
+++ src/main/java/org/texastorque/constants/Constants.java
@@ -126,7 +126,7 @@ public class Constants {
     public static final double LIMELIGHT_DISTANCE_OFFSET = 12 * INCH_TO_FOOT * FOOT_TO_METER;
     public static final Translation2d HUB_CENTER_POSITION = new Translation2d(8.2, 4.1);
 
-    public static final double IDLE_SHOOTER_PERCENT = .1;
+    public static final double IDLE_SHOOTER_PERCENT = .3;
     public static final double SHOOTER_REDUCTION = 5 / 3.;
 
 }

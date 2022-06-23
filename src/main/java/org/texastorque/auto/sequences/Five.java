7f7fe38fe18ac3e2a810b5a310ace79780346323
Juicestus <jus@gtsbr.org>
2022-06-22 19:06:28 -0500

    maybe fixed another bug

--- src/main/java/org/texastorque/auto/sequences/Five.java
+++ src/main/java/org/texastorque/auto/sequences/Five.java
@@ -3,6 +3,7 @@ package org.texastorque.auto.sequences;
 import org.texastorque.Subsystems;
 import org.texastorque.auto.commands.*;
 import org.texastorque.subsystems.Magazine.BeltDirection;
+import org.texastorque.subsystems.Magazine.GateDirection;
 import org.texastorque.torquelib.auto.*;
 import org.texastorque.torquelib.auto.commands.*;
 
@@ -13,13 +14,13 @@ public class Five extends TorqueSequence implements Subsystems {
     protected void init() {
         addBlock(new TorqueBlock(
                 new FollowPathPlanner("Five1", true, 2, 1),
-                new Execute(() -> { 
-                    magazine.setBeltDirection(BeltDirection.INTAKING); 
-                })
+                new Execute(() -> { magazine.setBeltDirection(BeltDirection.INTAKING); })
         ));
         addBlock(new TorqueBlock(new Shoot(1700, 26, 172.15, true, 2)));
+        // addBlock(new TorqueBlock(new Execute(() -> { magazine.setGateDirection(GateDirection.OFF); })));
         // addBlock(new TorqueBlock(new FollowPathPlanner("Five2", false, 2, 1)));
         // addBlock(new TorqueBlock(new Wait(1.)));
         // addBlock(new TorqueBlock(new FollowPathPlanner("Five3", false, 2, 1)));
+        addBlock(new TorqueBlock(new Execute(() -> { magazine.setBeltDirection(BeltDirection.OFF); })));
     }
 }

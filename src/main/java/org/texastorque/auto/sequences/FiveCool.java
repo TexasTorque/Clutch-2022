56da98bd9bd3d226e4fde547917c72c5b7bb8406
Juicestus <jus@gtsbr.org>
2022-08-10 20:37:29 -0500

    Fun funky fresh five ball

--- src/main/java/org/texastorque/auto/sequences/FiveCool.java
+++ src/main/java/org/texastorque/auto/sequences/FiveCool.java
@@ -1,12 +1,8 @@
 package org.texastorque.auto.sequences;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-
 import org.texastorque.Subsystems;
-import org.texastorque.auto.commands.Creep;
 import org.texastorque.auto.commands.Path;
 import org.texastorque.auto.commands.Shoot;
-import org.texastorque.auto.commands.Target;
 import org.texastorque.subsystems.Intake.IntakeState;
 import org.texastorque.subsystems.Magazine.BeltDirection;
 import org.texastorque.subsystems.Magazine.GateDirection;
@@ -15,59 +11,67 @@ import org.texastorque.subsystems.Turret.TurretState;
 import org.texastorque.torquelib.auto.TorqueBlock;
 import org.texastorque.torquelib.auto.TorqueSequence;
 import org.texastorque.torquelib.auto.commands.TorqueExecute;
-import org.texastorque.torquelib.auto.commands.TorqueWait;
 
 public class FiveCool extends TorqueSequence implements Subsystems {
     public FiveCool() {
-        final double firstTurret = 165;
+        final double warmupDelta = 300;
+
+        // Turn on subsystems and shoot the first ball
+
+        final double rpm1 = 1500, hood1 = 15, turret1 = 165;
+     
         addBlock(new TorqueBlock(new TorqueExecute(() -> {
-                                    //  magazine.setBeltDirection(BeltDirection.INTAKING);
-                                     intake.setState(IntakeState.INTAKE);
+            intake.setState(IntakeState.INTAKE);
+            turret.setState(TurretState.POSITIONAL);
+            turret.setPosition(turret1);
+            shooter.setState(ShooterState.WARMUP);
+            shooter.setFlywheelSpeed(rpm1 - warmupDelta);
+            shooter.setHoodPosition(hood1);
+        })));
 
-                                     turret.setState(TurretState.POSITIONAL);
-                                     turret.setPosition(firstTurret);
+        addBlock(new TorqueBlock(new Shoot(rpm1, hood1, turret1, true, 1)));
 
-                                     shooter.setState(ShooterState.WARMUP);
-                                     shooter.setFlywheelSpeed(1000);
-                                     shooter.setHoodPosition(15);
-                                 })));
+        // Pick up and shoot the second and third balls
 
-        addBlock(new TorqueBlock(new Shoot(1450, 15, firstTurret, false, 1)));
+        final double rpm2 = 1900, hood2 = 20, turret2 = -115;
+
+        addBlock(new TorqueBlock(new TorqueExecute(() -> {
+            magazine.setGateDirection(GateDirection.OFF);
+            intake.setState(IntakeState.INTAKE);
+            turret.setState(TurretState.POSITIONAL);
+            turret.setPosition(turret2);
+            shooter.setState(ShooterState.WARMUP);
+            shooter.setFlywheelSpeed(rpm2 - warmupDelta);
+            shooter.setHoodPosition(hood2);
+        })));
+
         addBlock(new TorqueBlock(new Path("Five1", true, 4, 2)));
-        final double turret2 = -115, hood2 = 20;
-        addBlock(new TorqueBlock(new TorqueExecute(() -> {
-
-                                    //  magazine.setBeltDirection(BeltDirection.INTAKING);
-                                     magazine.setGateDirection(GateDirection.OFF);
-                                    intake.setState(IntakeState.INTAKE);
-
-                                    turret.setState(TurretState.POSITIONAL);
-                                    turret.setPosition(turret2);
-
-                                    shooter.setState(ShooterState.WARMUP);
-                                    shooter.setFlywheelSpeed(1000);
-                                    shooter.setHoodPosition(hood2);
-                                })));
         addBlock(new TorqueBlock(new Shoot(1900, hood2, turret2, false, 1.8)));
-        // addBlock(new TorqueBlock(new Path("Five2", false, 4, 2)));
-        // addBlock(new TorqueBlock(new TorqueExecute(() -> {
-        //     turret.setState(TurretState.POSITIONAL);    
-        //     turret.setPosition(-65);
 
-        //     shooter.setState(ShooterState.WARMUP);
-        //     shooter.setFlywheelSpeed(800);
-        //     shooter.setAutoOffset(0);
-        // })));
-        // addBlock(new TorqueBlock(new Path("Five3", false, 4, 2)));
-        // addBlock(new TorqueBlock(new Target(false, .5)));
-        // addBlock(new TorqueBlock(new Creep(2, new ChassisSpeeds(-.5, 0, 0)), 
-        //         new Target(false, 4)));
+        // Pick up balls at human player
 
-        // addBlock(new TorqueBlock(new TorqueExecute(() -> {
-        //     turret.setOffset(0);
-        //     magazine.setBeltDirection(BeltDirection.OFF);
-        //     intake.setState(IntakeState.PRIMED);
-        //     turret.setState(TurretState.CENTER);
-        // })));
+        final double rpm3 = 1900, hood3 = 20, turret3 = -180;
+
+        addBlock(new TorqueBlock(new TorqueExecute(() -> {
+            magazine.setGateDirection(GateDirection.OFF);
+            intake.setState(IntakeState.INTAKE);
+            turret.setState(TurretState.POSITIONAL);
+            turret.setPosition(turret3);
+            shooter.setState(ShooterState.WARMUP);
+            shooter.setFlywheelSpeed(rpm3 - warmupDelta);
+            shooter.setHoodPosition(hood3);
+        })));
+
+        addBlock(new TorqueBlock(new Path("Five3", true, 4, 2)));
+        addBlock(new TorqueBlock(new Shoot(rpm3, hood3, turret3, true, 1.8)));
+
+        // Turn off subsystems
+
+        addBlock(new TorqueBlock(new TorqueExecute(() -> {
+            turret.setOffset(0);
+            magazine.setBeltDirection(BeltDirection.OFF);
+            intake.setState(IntakeState.PRIMED);
+            turret.setState(TurretState.CENTER);
+        })));
     }
 }
